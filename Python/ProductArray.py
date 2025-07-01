ab56e5ccd5dd30c80611b277cf9430e37ff6b45f
Manuel <manuelmhs114@gmail.com>
2025-07-01 19:16:30 -0300

    Product Array Puzzle

---
+++ Python/ProductArray.py
@@ -0,0 +1,39 @@
+"""
+Given an array arr[], construct a product array res[], where each element in res[i] is the product of all elements in arr[] except arr[i].
+Return this resultant array, res[].
+arr.size() >= 2
+"""
+
+import sys
+def main():
+    arr = list(map(int, sys.argv[1:]))
+    total = 1
+    zeroesCount = 0
+
+    for n in arr:
+        if n == 0 and zeroesCount == 0:
+            zeroesCount += 1
+            continue
+        elif n == 0 and zeroesCount == 1:
+            zeroesCount += 1
+            total = 0
+            break
+        else:
+            total *= n
+    
+
+    res = [None] * len(arr)
+    for i, n in enumerate(arr):
+        if n == 0:
+            res[i] = total
+        else:
+            if zeroesCount == 0:
+                res[i] = total // n
+            elif zeroesCount >= 1:
+                res[i] = 0
+
+    print(res)
+    return res
+
+if __name__ == "__main__":
+    main()
--- tests/test_all.py
+++ tests/test_all.py
@@ -4,6 +4,7 @@ from Python.FirstUniqueChar import main as FirstUniqueChar
 from Python.NearestZeroSumTwo import main as NearestZeroSumTwo
 from Python.AngleHourAndMinute import main as AngleHourAndMinute
 from Python.NthEvenFibNum import main as NthEvenFibNum
+from Python.ProductArray import main as ProductArray
 
 #would be safer to use monkeypatch to change sys.argv
 
@@ -23,37 +24,47 @@ def test_NearestZeroSumTwo():
     sys.argv = ["", "-10", "-3", "1", "4", "8"]
 
     r1 = NearestZeroSumTwo()
-
     assert r1 == (-3,4) or r1 == (4,-3)
 
     sys.argv = ["", "4", "7", "2"]
 
     r2 = NearestZeroSumTwo()
-
     assert r2 == (4,2) or r2 == (2,4)
 
 def test_AngleHourAndMinute():
     sys.argv = ["", "3", "30"]
 
     r1 = AngleHourAndMinute()
-
     assert r1 == 75
 
     sys.argv = ["", "9", "0"]
 
     r2 = AngleHourAndMinute()
-
     assert r2 == 90
 
 def test_NthEvenFibNum():
     sys.argv = ["", "1"]
 
     r1 = NthEvenFibNum()
-
     assert r1 == 2
 
     sys.argv = ["", "4"]
 
     r2 = NthEvenFibNum()
-
     assert r2 == 144
+
+def test_ProductArray():
+    sys.argv = ["", "10", "3", "5", "6", "2"]
+
+    r1 = ProductArray()
+    assert r1 == [180, 600, 360, 300, 900]
+
+    sys.argv = ["", "12", "0"]
+
+    r2 = ProductArray()
+    assert r2 == [0, 12]
+
+    sys.argv = ["", "1", "0", "7", "0", "-2"]
+
+    r3 = ProductArray()
+    assert r3 == [0, 0, 0, 0, 0]
